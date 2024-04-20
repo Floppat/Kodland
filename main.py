3f1c6ec005d057026b3ec76db485773368b479be
Floppat <164739638+Floppat@users.noreply.github.com>
2024-04-21 01:33:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='/', intents = discord.Intents.all())
 
-coins = 0
-win = 10
-lose = 1
+
 pet = {
     'здоровье': 100,
     'сила': 2,
@@ -28,74 +26,6 @@ async def cmd(message):
     await message.send('попробуйте /sleep  (полное восстановление выносливости)')
     await message.send('попробуйте /cmd')
     await message.send('попробуйте /guide  (ввод в игру)')
-    await message.send('попробуйте /shop  (покупка артефактов)')
-@bot.command('shop')
-async def shop(message):
-    await message.send('/s400;   400 монет    5 защиты')   
-    await message.send('/s800;   800 монет   10 защиты') ##########################################################################################################################
-    await message.send('/s1200;  1200 монет  15 защиты')
-    await message.send('/s1600;  1600 монет  20 защиты')
-    await message.send('/s2000;  2000 монет  25 защиты')
-@bot.command('s400')
-global coins
-async def s400(message):
-    if coins >= 400:
-        coins = coins - 400
-        pet['защита'] = 5
-        await message.send('остаток денег:') 
-        await message.send(coins)
-        await message.send(str(pet))
-    else:
-        await message.send('денег не хватает') 
-        await message.send(str(pet))
-@bot.command('s800')
-global coins
-async def s800(message):
-    if coins >= 800:
-        coins = coins - 800
-        pet['защита'] = 10
-        await message.send('остаток денег:') 
-        await message.send(coins)
-        await message.send(str(pet))
-    else:
-        await message.send('денег не хватает') 
-        await message.send(str(pet))
-@bot.command('s1200')
-global coins
-async def s1200(message):
-    if coins >= 1200:
-        coins = coins - 1200
-        pet['защита'] = 15
-        await message.send('остаток денег:') 
-        await message.send(coins)
-        await message.send(str(pet))
-    else:
-        await message.send('денег не хватает') 
-        await message.send(str(pet))
-@bot.command('s1600')
-global coins
-async def s1600(message):
-    if coins >= 1600:
-        coins = coins - 1600
-        pet['защита'] = 20
-        await message.send('остаток денег:') 
-        await message.send(coins)
-        await message.send(str(pet))
-    else:
-        await message.send('денег не хватает') 
-        await message.send(str(pet))
-@bot.command('s2000')
-global coins
-async def s2000(message):
-    if coins >= 2000:
-        coins = coins - 2000
-        pet['защита'] = 25
-        await message.send('остаток денег:') 
-        await message.send(coins)
-        await message.send(str(pet))
-    else:
-        await message.send('денег не хватает') 
-        await message.send(str(pet))
 @bot.command('train')
 async def train(message):
     if pet['здоровье'] >=20 and pet['выносливость'] >= 40:
@@ -120,13 +50,12 @@ async def feed(message):
         await message.send(str(pet))
 
 @bot.command('fight')
-global lose,coins, win
 async def fight(message):
-    if pet['выносливость'] >=60 and pet['сила'] > lose:
+    if pet['выносливость'] >=60 and pet['сила'] > 5:
         enemy = {
             'здоровье' : random.randint (1,100),
-            'сила' : random.randint (lose, win),
-            'защита' : random.randint (1, lose)
+            'сила' : random.randint (1, 10),
+            'защита' : random.randint (1, 5)
         }
         pet['выносливость'] -=60
         await message.send('вы атакуете врага')
@@ -139,13 +68,6 @@ async def fight(message):
             await message.send('вы проиграли!')
         else:
             await message.send('вы выиграли!')
-            await message.send('вы заработали:')
-            coins = coins + 100
-            win = win + 2
-            lose = lose + 2
-            await message.send(coins)
-            await message.send('монет')
-            await message.send('вы выиграли. противник стал посильнее.')
     else:
         await message.send('сперва вашему питомцу следует восстановить силы')
 @bot.command('sleep')
